1f6ee384610ae1d27d3390abb483740017cbe2ce
Tquynguyen <136567785+Tquynguyen@users.noreply.github.com>
2023-06-14 14:33:02 +0700

    Add files via upload

---
+++ renluyenchuoi5.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+void Xoakhoangtrang(char *&str)
+	{int j=0;
+	for (int i=0;i<strlen(str)-1;i++)
+	{if (str[i]!=' ')
+	{str[j]=str[i];
+	j++;}
+	else if(j>0&&str[i+1]!=' ')
+	{str[j]=str[i];
+	j++;}
+	}
+	str[j]='\0';}
+char *layten(char *str)
+	{int k;
+	for (int i=strlen(str)-1;i>=0;i--)
+	{if (str[i]==' ')
+	{ k=i+1;
+	break;}
+	}
+	int i2=0;
+	char *strTen=new char[35];
+	for (int i=k;i<strlen(str);i++)
+	{strTen[i2]=str[i];
+	i2++;
+	}
+	strTen[i2]='\0';
+	return strTen;
+	}
+char *layho(char *str)
+	{int k;
+	for (int i=0;i<strlen(str);i++)
+	{if (str[i]==' ')
+	{ k=i;
+	break;}
+	}
+	int i2=0;
+	char *strHo=new char[35];
+	for (int i=0;i<k;i++)
+	{strHo[i2]=str[i];
+	i2++;
+	}
+	strHo[i2]='\0';
+	return strHo;
+	}
+	
+int main(int argc, char** argv) {
+	char *str=new char [255];
+	strcpy(str,"    Nguyen              Thi                  Lung             Linh        Long  Lanh             ");
+	cout<<"chuoi ban dau la:"<<str<<endl;
+	cout<<"chieu dai chuoi ban dau la:"<<strlen(str)<<endl;
+	Xoakhoangtrang(str);
+	cout<<"chuoi sau khi xoa khoang trang la:"<<str<<endl;
+	cout<<"chieu dai chuoi sau khi xoa khoang trang la:"<<strlen(str)<<endl;
+	char *ten=layten(str);
+	cout<<"Ten = "<<ten<<endl;
+	cout<<"do dai cua ten la:"<<strlen(ten)<<endl;
+	char *ho=layho(str);
+	cout<<"Ho = "<<ho<<endl;
+	cout<<"do dai cua ten la:"<<strlen(ho)<<endl;
+	return 0;
+}
